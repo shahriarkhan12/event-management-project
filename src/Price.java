7b823f15d656a9280e2716c15b1e910b90d86cfd
shahriarkhan12 <65500749+shahriarkhan12@users.noreply.github.com>
2023-08-19 15:30:15 +0600

    Create Price.java

---
+++ src/Price.java
@@ -0,0 +1,42 @@
+static public class price
+    {
+        private int priceID;
+        String amount;
+        String description;
+
+        price(int priceID ,String amount , String description)
+        {
+            this.amount = amount;
+            this.description = description;
+
+        }
+
+        public int getPriceID()
+        {
+            return priceID;
+        }
+
+        public void setPriceID(int priceID)
+        {
+            this.priceID = priceID;
+
+
+        }
+
+
+        void create()
+        {
+
+
+
+        }
+
+        void modify()
+        {
+
+
+
+        }
+
+
+    }
